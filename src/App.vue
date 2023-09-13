35afa2cb6cc2771559486aae64a0f99de8b1e793
Gilles FRANCOIS <gilles@sopress.net>
2023-09-13 16:33:37 +0200

    texte

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@ import Trombinoscope from '@/components/Trombinoscope.vue';
   <!-- <NavBar />-->
   <Trombinoscope />
   <footer>
-    Choisissez votre polaroïd sur le site du coworking, dans le menu "Mon compte".<br>
-    <b style="color:red">En rouge: Solde négatif ou adhésion manquante</b>
+    <b style="color:red">En rouge: Solde négatif ou adhésion manquante</b><br>
+    Choisissez votre polaroïd sur le site du coworking, dans le menu "Mon compte".
   </footer>
 </template>
 
 <style>
 footer {
   padding: var(--marge);
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, .5) 100%);
+  color:white;
 }
 </style>
