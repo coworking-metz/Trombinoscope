9417132497070974e3ee0ee6ff2adef6ff082380
Gilles FRANCOIS <gilles@sopress.net>
2024-04-17 16:37:31 +0200

    Refresh automatique après 60 secondes

--- composants/head.php
+++ composants/head.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trombinoscope</title>
     <meta name="robots" content="noindex">
+    <meta http-equiv="refresh" content="60"> 
     <style>
         :root {
             --nb-users: <?= $total; ?>;
