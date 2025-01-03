69115082383e11c1dc08b3d25752e9c8f7e19b18
Coworking Metz <contact@coworking-metz.fr>
2025-01-03 14:40:39 +0100

    fond default

--- composants/fond.php
+++ composants/fond.php
@@ -1,3 +1,7 @@
-        <div class="fond">
+<div class="fond">
+<?php if( get_reglage('fond') ) {?>
     <img src="<?= get_reglage('fond/image', '/images/fond.jpg'); ?>" style="opacity:<?= get_reglage('fond/opacity',0); ?>;object-fit:<?= get_reglage('fond/mode'); ?>">
-</div>
+	<?php } else {?>
+		<img src="/images/fond.jpg">
+	<?php } ?>
+</div>	
